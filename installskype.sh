f32e317abbd0972d4874bb68895bc28b96ffa177
root <root@bt.(none)>
2015-08-13 05:26:47 -0400

    deleted file

--- installskype.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo apt-get install libqt4-dbus libqt4-network libqt4-xml libasound2
-wget http://www.skype.com/go/getskype-linux-beta-ubuntu-32
-sudo dpkg -i getskype-*
-sudo apt-get -f install
