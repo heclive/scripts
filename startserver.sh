690027aaff6eda8498993a7a09a3eefb8620fc05
root <hiyamobile@github.com>
2015-08-23 09:19:39 -0400

    mod

--- startserver.sh
+++ startserver.sh
@@ -5,14 +5,14 @@ clear
 			# Quelliste fuer Packetmanager 
 
 IP="46.101.188.218"
-REP1="deb http://ftp.de.debian.org/debian squeeze main"
-REP2="" #deb http://http.kali.org/kali kali main non-free contrib"
-REP3="" #deb http://security.kali.org/kali-security kali/updates main contrib non-free"
-REP4="" #deb-src http://http.kali.org/kali kali main non-free contrib"
-REP5="" #deb-src http://security.kali.org/kali-security kali/updates main contrib non-free"
+WHITE="\033[1;29m"
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+GREEN="\033[1;32m"
+ENDCOLOR="\033[0m"
 
 echo ""
-echo "Allready Logged in to [46.101.188.218]? [y/n]"
+echo -e $WHITE"Allready Logged in to [46.101.188.218]? [y/n]"$ENDCOLOR
 read -s -n 1 log
 
 #=================================================================================================LOGGED IN?
@@ -30,14 +30,14 @@ read -s -n 1 log
 	else echo ""
 	fi
 #------------------------------------------------------------------------------------------------> ELSE
-echo "Checking Server status ...."
+echo -e $WHITE"Odoo Server status ...."$ENDCOLOR
 ps aux | grep odoo
 
 
 #=================================================================================================> START SERVER?
-echo "Please Choose:"
+echo -e $WHITE"Please Choose:"
 echo "1.) Start Server"
-echo "2.) Stop Server"
+echo -e "2.) Stop Server"$ENDCOLOR
 read  -s -n 1 start
 #Schleife
 	until [ $start = "1" -o $start = "2" ]
@@ -49,11 +49,12 @@ read  -s -n 1 start
 	if [ $start = "1" ]
 	then	sudo service odoo-server start
 		echo ""
-		echo "Odoo Server Started"		
+		echo -e $GREEN"Starting Odoo Server..."$ENDCOLOR		
 	else 
 		sudo service odoo-server stop
 		echo ""
-		echo "Odoo Server Stoped"
-echo ""
+		echo -e $RED"Stoping Odoo Server...."$ENDCOLOR
 	fi			
+sleep 0.5
+ps aux | grep odoo 
 
